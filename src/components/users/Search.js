3fbd95a6551da4f3ae09900d0cebb9b0d9297a26
Pratik Lodha <pratiklodha01@gmail.com>
2019-12-13 10:59:29 +0530

    alert component

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,8 +1,16 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Search extends Component {
     state = {
         text: ''
+    };
+
+    static propTypes = {
+        searchUsers: PropTypes.func.isRequired,
+        clearUsers: PropTypes.func.isRequired,
+        showClear: PropTypes.bool.isRequired,
+        setAlert: PropTypes.func.isRequired
     };
 
     onChange = (e) => {
@@ -11,16 +19,24 @@ class Search extends Component {
 
     onSubmit = (e) => {
         e.preventDefault();
-
+        if(this.state.text === '') {
+            this.props.setAlert('Please enter something', 'light');
+        } else {
+            this.props.searchUsers(this.state.text);
+            this.setState({text: ''});
+        }
     };
 
     render() {
+        const {showClear, clearUsers} = this.props;
         return (
             <div>
                 <form onSubmit={this.onSubmit} className="form">
-                    <input type="text" name="text" placeholder="Search Users..." value={this.state.text} onChange={this.onChange}/>
+                    <input type="text" name="text" placeholder="Search users..." value={this.state.text} onChange={this.onChange}/>
                     <input type="submit" value="Search" className="btn btn-dark btn-block" />
                 </form>
+                {showClear && <button className="btn btn-light btn-block" onClick={clearUsers}>Clear</button>}
+                
             </div>
         )
     }
