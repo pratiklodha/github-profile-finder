21fecfae3a97f086dffcc6699bc244e1bb1ae84e
Pratik Lodha <pratiklodha01@gmail.com>
2019-12-15 13:03:52 +0530

    form autocomplete off

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -21,7 +21,7 @@ const Search = ({searchUsers, showClear, clearUsers, setAlert}) => {
     return (
         <div>
             <form onSubmit={onSubmit} className="form">
-                <input type="text" name="text" placeholder="Search users..." value={text} onChange={onChange}/>
+                <input type="text" name="text" placeholder="Search users..." value={text} onChange={onChange} autoComplete="off"/>
                 <input type="submit" value="Search" className="btn btn-dark btn-block" />
             </form>
             {showClear && <button className="btn btn-light btn-block" onClick={clearUsers}>Clear</button>}
