7fefce163aee82267222cf1ea3b30315bb9ab820
Pratik Lodha <pratiklodha01@gmail.com>
2019-12-15 16:57:53 +0530

    Updates

--- precache-manifest.f3192d5cdde8c1352fbdc1d6b5799aca.js
+++ precache-manifest.f3192d5cdde8c1352fbdc1d6b5799aca.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "97ff7538a10cd0eb8f4ce074ef0c6188",
+    "revision": "f661bb310a8d4c66221c6d28bee4ce43",
     "url": "/github-profile-finder/index.html"
   },
   {
-    "revision": "def131354d75dc4cc21e",
+    "revision": "13bf85da3bef3fa02ea6",
     "url": "/github-profile-finder/static/css/main.f65227a3.chunk.css"
   },
   {
-    "revision": "b3c7a1e61d91a97a68bc",
-    "url": "/github-profile-finder/static/js/2.9dc2e6ec.chunk.js"
+    "revision": "9c9902cf31e269486bb1",
+    "url": "/github-profile-finder/static/js/2.856a5e38.chunk.js"
   },
   {
     "revision": "84a2a7a390df1ac99fa4fadf23155e60",
-    "url": "/github-profile-finder/static/js/2.9dc2e6ec.chunk.js.LICENSE"
+    "url": "/github-profile-finder/static/js/2.856a5e38.chunk.js.LICENSE"
   },
   {
-    "revision": "def131354d75dc4cc21e",
-    "url": "/github-profile-finder/static/js/main.4ff9cf5d.chunk.js"
+    "revision": "13bf85da3bef3fa02ea6",
+    "url": "/github-profile-finder/static/js/main.e9c8d2b9.chunk.js"
   },
   {
     "revision": "c48095d6810b5976db5a",
